debd9e560f3029f8b794a557de67bc564a79c902
changhwan <chang5hustle@gmail.com>
2022-02-26 01:02:45 +0900

    [feat] gradle 세팅 및 기본세팅

---
+++ app/src/main/java/com/example/changmvvmstudy/ui/home/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.changmvvmstudy.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.changmvvmstudy.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
